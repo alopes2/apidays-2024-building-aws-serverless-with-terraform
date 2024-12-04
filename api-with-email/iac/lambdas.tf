c3bb046b5d470b9ffcc8ac38b8999adc54ad8106
Andre Lopes <andrevitorlopes@gmail.com>
2024-12-04 16:21:48 +0100

    fix

--- api-with-email/iac/lambdas.tf
+++ api-with-email/iac/lambdas.tf
@@ -1,6 +1,6 @@
 module "get_movie_lambda" {
   source           = "./modules/lambda"
   name             = "get-movie"
-  source_file_path = "./lambda_init_code/index.mjs"
+  source_file_path = "./lambda_init_code/index.js"
   policies         = [data.aws_iam_policy_document.get_movie_item.json]
 }
